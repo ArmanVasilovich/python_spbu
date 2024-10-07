36b00c78fe15f573800e030370f45b228c9d64bc
ArmanVasilovich <arman.lu.88@mail.ru>
2024-10-07 14:02:43 +0300

    List Comprehension

--- 7th-October.py
+++ 7th-October.py
@@ -2,6 +2,7 @@
 # myList = [1, 2, 3, 4, 5]
 # myList.append(6)
 # print(myList)
+from random import random
 
 # Добавление массива цифр от 1 до 100
 # nums = []
@@ -46,15 +47,29 @@
 # FourthList = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 # print(FourthList[0], FourthList[0][1])
 
-# Матрица
-count = 1
-matrix = []
-for i in range(7):
-    matrix.append(list(range(count, count + 3)))
-    count += 3
+# # Матрица
+# count = 1
+# matrix = []
+# for i in range(7):
+#     matrix.append(list(range(count, count + 3)))
+#     count += 3
+#
+# # Вывод матрицы
+# for i in matrix:
+#     for j in i:
+#         print(j, end=' ')
+#     print()
 
-# Вывод матрицы
-for i in matrix:
-    for j in i:
-        print(j, end=' ')
-    print()
+# List Comprehension
+# squares = []
+# for i in range(1, 11):
+#     squares.append(i**2)
+# print(squares)
+
+# List Comprehension (В одну строчку)
+# squares = [i**10 for i in range(1, 11)]
+# print(squares)
+
+# # List Comprehension (В одну строчку с условием)
+# squares = [i**2 if i%2 != 0 else i for i in range(1, 11)]
+# print(squares)
