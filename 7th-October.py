1782bd2e646ddc2e5af90b563952db982193e9bb
ArmanVasilovich <arman.lu.88@mail.ru>
2024-10-07 11:50:03 +0300

    First tasks

---
+++ 7th-October.py
@@ -0,0 +1,16 @@
+# Добавление элемента в конец списка
+# myList = [1, 2, 3, 4, 5]
+# myList.append(6)
+# print(myList)
+
+# Добавление массива цифр от 1 до 100
+nums = []
+for i in range(1, 101):
+    nums.append(i)
+
+# word = input('Введите слово: ')
+# wordList = list(word)
+# print(wordList)
+
+
+
