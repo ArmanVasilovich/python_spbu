16d7fc9cf81eb545581cef6a507088578f8ab05a
ArmanVasilovich <arman.lu.88@mail.ru>
2024-10-07 12:04:16 +0300

    Методы списка (Удаление)

--- 7th-October.py
+++ 7th-October.py
@@ -4,16 +4,28 @@
 # print(myList)
 
 # Добавление массива цифр от 1 до 100
-nums = []
-for i in range(1, 101):
-    nums.append(i)
+# nums = []
+# for i in range(1, 101):
+#     nums.append(i)
 
 # word = input('Введите слово: ')
 # wordList = list(word)
 # print(wordList)
 
-# Сортировка
-nums2 = [2, 13, 44, 9, 88]
+# Сортировка + Reverse
+nums2 = [2, 13, 44, 9, 88, 9]
+nums2.sort(reverse=True)
 sortNumns = sorted(nums2)
+
+# Подсчет одинаковых элементов в списке
+# print(sortNumns.count(9))
+
+# # Удаляет по значению без возвращения самого значения
+# sortNumns.remove(9)
+# # Удаление по индексу с возвращением значения
+# deletedNum = sortNumns.pop(1)
+# print(sortNumns, deletedNum)
+
+
 
 
